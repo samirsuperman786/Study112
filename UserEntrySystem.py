75d15593b3e01400e59fa4e3a7ccaf4499e867f5
Alvin Shek <ashek@andrew.cmu.edu>
2018-04-07 10:52:35 -0400

    Add files via upload
    almost functional, still ugly

--- UserEntrySystem.py
+++ UserEntrySystem.py
@@ -1,35 +1,34 @@
 from tkinter import *
 
 def runCode(event=None):
-    global userResult
-    result = userResult.split("\n")
+    global userResult, textEntry
+    userResult = textEntry.get()
+    textEntry.set(userResult)
+    result = userResult.split(",")
     print(result)
     
 def newLine(event=None):
-    global userResult
-    userResult = userResult + "\n"
-    return userResult
+    global userResult, textEntry
+    userResult = textEntry.get() + ","
+    textEntry.set(userResult)
     
 def run(width=300,height=300):
-    global userResult
+    global userResult, textEntry
     root = Tk()
     canvas = Canvas(root, width=width, height=height)
     canvas.pack()
     #checkCodeScreen(canvas,data)
     textEntry = StringVar()
     textEntry.set(userResult)
-    userResult = textEntry.get()
     textBox = Entry(canvas,textvariable=textEntry)
     canvas.create_window(width//2, height//2, 
         window=textBox, height=height//3, width=width//2)
     checkSol = Button(canvas,text="Check Solution", command=runCode)
     checkSol.configure(width = 10, activebackground = "#33B5E5", relief = FLAT)
     canvas.create_window(10, 10, anchor=NW, window=checkSol)
-    
     textBox.bind('<Return>', newLine)
-    userResult = newLine()
     root.mainloop()
-global userResult
+
 userResult = "Stay Gold Ponyboy"
 run()
 
